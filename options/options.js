6af7c68276002d846a666f7dea82648afcf1ae89
Crowdbotics <team@crowdbotics.com>
2021-08-23 18:22:07 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,6 @@
+export const globalOptions = {
+  name: "empty_cake_30052Identifier",
+  url: "https://empty_cake_30052Identifier.botics.co",
+  api: "https://empty_cake_30052Identifier.botics.co/api/v1"
+}
+export const modulesOptions = {}
